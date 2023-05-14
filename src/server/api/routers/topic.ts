fb199410490e3e47856b7f9b6c6c2ae684942105
Richard Schmidt <richardnew12511@gmail.com>
2023-05-14 17:53:34 -0500

    topics coming back from server

--- src/server/api/routers/topic.ts
+++ src/server/api/routers/topic.ts
@@ -3,26 +3,13 @@ import { createTRPCRouter, publicProcedure } from "../trpc";
 import { TRPCError } from "@trpc/server";
 
 export const topicRouter = createTRPCRouter({
-  getAllForUserId: publicProcedure
-    .input(z.object({ userId: z.string() }))
-    .query(({ ctx, input }) => {
-      // if (!user) throw new TRPCError({ code: "NOT_FOUND" });
-      // console.log(`in query, userId is:${user.id}`);
-      // return ctx.prisma.topic.findMany({
-      //   where: {
-      //     userId: input.userId,
-      //   },
-      // });
-    }),
   getAll: publicProcedure.query(({ ctx }) => {
-    return { greeting: `Hello ${ctx.userId}` };
-    // const { user } = useUser();
-    // if (!user) throw new TRPCError({ code: "NOT_FOUND" });
-    // console.log(`in query, userId is:${user.id}`);
-    // return ctx.prisma.topic.findMany({
-    //   where: {
-    //     userId: user.id,
-    //   },
-    // });
+    if (!ctx.userId) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    return ctx.prisma.topic.findMany({
+      where: {
+        userId: ctx.userId,
+      },
+    });
   }),
 });
