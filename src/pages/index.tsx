c968a39bef094378353bc66f3a61031265d23965
Richard Schmidt <richardnew12511@gmail.com>
2023-05-17 18:55:18 -0500

    preline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,17 @@ const Home: NextPage = () => {
       <main className="flex h-screen flex-col">
         <Header />
         <div className="overflow-none grid h-full max-h-screen w-full grid-cols-10 gap-3 p-3">
-          <div className="col-span-2 flex h-full flex-col border border-rose-500">
-            <div className="flex-1">Topics</div>
-            <div className="flex-none">Add Topic</div>
+          <div className="card col-span-2 flex h-full flex-col border border-rose-500">
+            <div className="card-body flex flex-col">
+              <div className="flex-1">Topics</div>
+              <div className="flex-none">Add Topic</div>
+            </div>
           </div>
-          <div className="col-span-8 border border-slate-600">Notes List</div>
+          <div className="card col-span-8 border border-slate-600">
+            <div className="card-body">
+              <h2 className="card-title">Notes</h2>
+            </div>
+          </div>
         </div>
       </main>
     </>
